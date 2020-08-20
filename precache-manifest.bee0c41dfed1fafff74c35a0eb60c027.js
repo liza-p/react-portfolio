3cfae6d9f1e8dfb9e7f4f2540ac89fc6dd7a5fe8
liza p <liyourlp@gmail.com>
2020-08-19 22:16:51 -0700

    Updates

---
+++ precache-manifest.bee0c41dfed1fafff74c35a0eb60c027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caa296b57c3b21c9f29450897397c463",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a477372d867321465581",
+    "url": "/react-portfolio/static/css/main.e398b798.chunk.css"
+  },
+  {
+    "revision": "b2d8ab45d419d9964e64",
+    "url": "/react-portfolio/static/js/2.25af06fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.25af06fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a477372d867321465581",
+    "url": "/react-portfolio/static/js/main.1fec64b0.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
